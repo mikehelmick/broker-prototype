9692948d9580c385b7744c74706e2b1cadc906d5
Mike Helmick <helmick@google.com>
2022-11-02 11:32:53 -0700

    upgrade some deps so that this runs again

--- broker/mix.exs
+++ broker/mix.exs
@@ -32,13 +32,13 @@ defmodule Broker.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5.0"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:plug_cowboy, "~> 2.1"},
       {:elixir_uuid, "~> 1.2" },
       {:httpoison, "~> 1.4"}
     ]
